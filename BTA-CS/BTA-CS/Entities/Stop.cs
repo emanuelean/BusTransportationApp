baba768b449a8f08d04bf5a66ce2432263c7c907
Mihai-Alexandru Erbasu <erbasualex@gmail.com>
2018-03-02 20:04:49 +0100

    Base
    Basic Scheleton of the Application.

---
+++ BTA-CS/BTA-CS/Entities/Stop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BTA_CS.Entities
+{
+    public class Stop : IEntity
+    {
+        public int ID { get; set; }
+
+        public String Name { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Bus Transportation App
+
+Microsoft SQL Server Management Studio Download: https://docs.microsoft.com/en-us/sql/ssms/download-sql-server-management-studio-ssms (First link)
+IIS: https://msdn.microsoft.com/en-us/library/ms181052(v=vs.80).aspx
