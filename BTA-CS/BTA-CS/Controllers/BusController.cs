101862204fdf07b9115c74cda2f0b2ca98b7a48e
Mihai-Alexandru Erbasu <erbasualex@gmail.com>
2018-03-21 15:59:03 +0100

    Small Errors
    Some Errors solved

---
+++ BTA-CS/BTA-CS/Controllers/BusController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Net;
+using System.Threading.Tasks;
+using BTA_CS.Entities;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace BTA_CS.Controllers
+{
+    public class BusController : ApiController
+    {
+        private BTAContext db = new BTAContext();
+
+        // GET: api/Bus
+        public IQueryable<Bus> GetBuses()
+        {
+            return db.Buses;
+        }
+
+        // GET: api/Buses/5
+        [ResponseType(typeof(Bus))]
+        public async Task<IHttpActionResult> GetBus(int id)
+        {
+            Bus bus = await db.Buses.FindAsync(id);
+            if (bus == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bus);
+        }
+
+        // PUT: api/Buses/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutBus(int id, Bus bus)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bus.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(bus).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BusExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Buses
+        [ResponseType(typeof(Bus))]
+        public async Task<IHttpActionResult> PostBus(Bus bus)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Buses.Add(bus);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = bus.ID }, bus);
+        }
+
+        // DELETE: api/Buses/5
+        [ResponseType(typeof(Bus))]
+        public async Task<IHttpActionResult> DeleteBus(int id)
+        {
+            Bus bus = await db.Buses.FindAsync(id);
+            if (bus == null)
+            {
+                return NotFound();
+            }
+
+            db.Buses.Remove(bus);
+            await db.SaveChangesAsync();
+
+            return Ok(bus);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BusExists(int id)
+        {
+            return db.Buses.Count(e => e.ID == id) > 0;
+        }
+    }
+}
--- BTA-CS/BTA-CS/Entities/Stop.cs
+++ BTA-CS/BTA-CS/Entities/Stop.cs
@@ -12,5 +12,8 @@ namespace BTA_CS.Entities
         public String Name { get; set; }
         public float Latitude { get; set; }
         public float Longitude { get; set; }
+
+        public int BusID { get; set; }
+        public Bus Bus { get; set; }
     }
 }
